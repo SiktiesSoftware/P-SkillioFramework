5c1c31de3bea9930475bc02fdc050c53e44e989b
Damien Loup <damien.loup@eduvaud.ch>
2022-12-22 14:48:56 +0100

    redirection with <a> links using route.getByname->link

--- 02-SourceCode/pages/includes/nav.php
+++ 02-SourceCode/pages/includes/nav.php
@@ -1,6 +1,6 @@
 <nav>
     NAV.PHP <br>
-    <a href="">home</a>  <br>
+    <a href=<?= Route::GetRouteByName("contact")->link; ?>>home</a>  <br>
     <a href="">contact</a>  <br>
     <a href="">user</a>  <br>
     <a href="">users</a>  <br>
